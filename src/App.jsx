66752dad6bda25bfbbe1eddeed5983090d71bd1c
nunez25 <jmmorenonunez2024@hccci.edu.ph>
2026-09-11 12:28:33 +0800

    Task 6:add 404 catch-all route

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import HomePage from "./pages/HomePage";
 import CountriesPage from "./pages/CountriesPage";
 import BucketListPage from "./pages/BucketListPage";
 import AboutPage from "./pages/AboutPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 const App = () => {
   // TODO: replace this with a real route table using react-router.
@@ -17,6 +18,7 @@ const App = () => {
         <Route path="countries" element={<CountriesPage />} />
         <Route path="bucket-list" element={<BucketListPage />} />
         <Route path="about" element={<AboutPage />} />
+        <Route path="*" element={<NotFoundPage/>}/>
       </Route>
     </Routes>
   );
